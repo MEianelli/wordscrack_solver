124601f100025af2f0e402520e304b5c7c0bed1d
MEianelli <filipelouzano@gmail.com>
2017-09-13 12:50:53 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        <title>Words Game Debug</title>
+        <link href="style.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <form method="POST">
+            Type in all the letters:
+            <input id="text" type="text" name="text" autofocus maxlength="16"/>
+            <input id="send" type="submit" value="send"/>
+        </form>
+        <div id="result"></div>
+        <div id="panel">
+            <div class="letter"></div>
+            <div class="letter"></div>
+            <div class="letter"></div>
+            <div class="letter"></div>
+            <div class="letter"></div>
+            <div class="letter"></div>
+            <div class="letter"></div>
+            <div class="letter"></div>
+            <div class="letter"></div>
+            <div class="letter"></div>
+            <div class="letter"></div>
+            <div class="letter"></div>
+            <div class="letter"></div>
+            <div class="letter"></div>
+            <div class="letter"></div>
+            <div class="letter"></div>
+        </div>
+        <div id="previous" class="controls">Previous</div>
+        <div id="stop" class="controls">Auto</div>
+        <div id="next" class="controls">Next</div>
+        <div id="reset" class="controls">Reset</div>
+        
+             
+        <script type="text/javascript" src="jquery-3.2.1.min.js"></script>
+        <script type="text/javascript" src="jQuery-Color.js"></script>
+        <script type="text/javascript" src="cracker.js"></script>
+    </body>
+</html>
+
